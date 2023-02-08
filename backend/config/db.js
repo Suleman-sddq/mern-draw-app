266cf6480b32d4d3967a46cd3c4b7eeb951584db
M.Suleman <sulemansddq@gmail.com>
2023-02-08 19:20:58 +0500

    updated DBconfig file

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,10 @@ const mongoose = require('mongoose');
 var connectDB = async () => {
    try {
       let conn;
-      if (process.env.DB === "cloud") {
-         mongoose.set("strictQuery", false);
-         conn = await mongoose.connect(process.env.MONGO_URI_CLOUD, { useNewUrlParser: true })
-      } else {
-         mongoose.set("strictQuery", false);
-         conn = await mongoose.connect(process.env.MONGO_URI_LOCAL, { useNewUrlParser: true })
-      }
+
+      mongoose.set("strictQuery", false);
+      conn = await mongoose.connect(process.env.MONGO_URI_CLOUD, { useNewUrlParser: true })
+
       console.log(`MongoDB connected: ${conn.connection.host}`)
    } catch (error) {
       console.log(error)
